cf5cd1190b20c86a2b02ecb983a6d51444358e5f
Isthimius <mike@adkinshouse.net>
2015-09-09 16:21:03 -0400

    ExpandedEnglish

---
+++ Expander.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpandedEnglish
+{
+    public class Expander : IExpander
+    {
+        private Dictionary<string, string> _conversions = new Dictionary<string,string>();
+
+        public string Expand(string sentenceToExpand)
+        {
+            var sb = new StringBuilder();
+            var words = sentenceToExpand.Split();
+
+            foreach (var word in words)
+            {
+                sb.Append(ExpandWord(word));
+                sb.Append(' ');
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        private void GetConversions()
+        {
+
+        }
+
+        private string ExpandWord(string word)
+        {
+            bool isCap = !string.IsNullOrWhiteSpace(word) && char.IsUpper(word, 0);
+            var wordLower = word.ToLower();
+
+            foreach (var conversion in _conversions)
+            {
+                if (wordLower.Contains(conversion.Key.ToLower()))
+                {
+                    wordLower.Replace(conversion.Key.ToLower(), conversion.Value);
+
+                    if (isCap)
+                        wordLower = wordLower.First().ToString().ToUpper() + wordLower.Substring(1);
+
+                    return wordLower;
+                }
+            }
+
+            return word;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpandedEnglish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IExpander expander = new Expander();
+
+            do
+            {
+                Console.WriteLine("Enter the sentence in need of expanding:");
+
+                var sentence = Console.ReadLine();
+                var expandedSentence = expander.Expand(sentence);
+                
+                Console.WriteLine(string.Format("expanded result: {0}", expandedSentence));
+                Console.WriteLine();
+
+                Console.WriteLine("Press {Esc} to exit.  Press any other key to continue.");
+                Console.WriteLine();
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
