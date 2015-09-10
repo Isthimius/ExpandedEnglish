2c87d1deb7e799ce0af7d8b5642e6c7770458073
Isthimius <mike@adkinshouse.net>
2015-09-10 04:49:29 -0400

    settings

---
+++ Config/Conversion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpandedEnglish.Config
+{
+    public class Conversion : ConfigurationElement
+    {
+        [ConfigurationProperty("From", IsRequired = true, IsKey = true)]
+        public string From
+        {
+            get { return (string)this["From"]; }
+            set { this["From"] = value; }
+        }
+
+        [ConfigurationProperty("To", IsRequired = true)]
+        public string To
+        {
+            get { return (string)this["To"]; }
+            set { this["To"] = value; }
+        }
+    }
+}
---
+++ Config/Conversions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpandedEnglish.Config
+{
+    [ConfigurationCollection(typeof(Conversion), AddItemName = "add", RemoveItemName = "remove", ClearItemsName = "clear")]
+    public class Conversions : ConfigurationElementCollection, IEnumerable<Conversion>
+    {
+        [ConfigurationProperty("LoadAtStartup", IsRequired = false, DefaultValue = true)]
+        public bool LoadAtStartup
+        {
+            get { return (bool)base["LoadAtStartup"]; }
+            set { base["LoadAtStartup"] = value; }
+        }
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new Conversion();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((Conversion)element).From;
+        }
+
+        public new IEnumerator<Conversion> GetEnumerator()
+        {
+            foreach (var id in this.BaseGetAllKeys())
+                yield return (Conversion)this.BaseGet(id);
+        }
+
+        public new Conversion this[string id]
+        {
+            get { return (Conversion)BaseGet(id); }
+        }
+
+        public void Add(Conversion file)
+        {
+            BaseAdd(file);
+        }
+
+        public void Clear()
+        {
+            BaseClear();
+        }
+
+        public void Remove(Conversion file)
+        {
+            BaseRemove(file);
+        }
+
+        public void RemoveAt(int index)
+        {
+            BaseRemove(index);
+        }
+    }
+}
